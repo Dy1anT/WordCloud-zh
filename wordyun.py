dcf212922f43614c4e241cd352903fd0d7678a06
Dy1aNT <30278281+Dy1aNT@users.noreply.github.com>
2017-11-09 10:54:16 +0800

    Create wordyun.py

---
+++ wordyun.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from wordcloud import WordCloud
+import jieba
+from scipy.misc import imread
+from os import path
+import matplotlib.pyplot as plt
+
+comment_text = open("word.txt",'r').read() #txt文件
+cut_text = ' '.join(jieba.cut(comment_text))
+color_mask = imread("pic.jpg") # 背景图片
+cloud = WordCloud(
+    font_path="msyh.ttf", #字体        
+    background_color='white', #背景色
+    mask=color_mask,
+    max_words=2000, #允许最大词汇
+    max_font_size=5000 #最大号字体
+)
+word_cloud = cloud.generate(cut_text)
+plt.imshow(word_cloud)
+plt.axis('off')
+plt.show()
+word_cloud.to_file("wordcloud.jpg")
