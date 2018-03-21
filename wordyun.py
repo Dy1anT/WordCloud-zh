9dda09f3ca3a5a7d5a74a1ac6d3ca48fab2110c6
Dy1aNT <jiaozhu_bbd!>
2018-03-21 20:34:28 +0800

    Update

--- wordyun.py
+++ wordyun.py
@@ -3,21 +3,26 @@
 from wordcloud import WordCloud
 import jieba
 from scipy.misc import imread
-from os import path
 import matplotlib.pyplot as plt
 
-comment_text = open("text.txt",'r').read() #txt文件
-cut_text = ' '.join(jieba.cut(comment_text))
-color_mask = imread("pic.jpg") # 背景图片
-cloud = WordCloud(
-    font_path="msyh.ttf", #字体        
-    background_color='white', #背景色
-    mask=color_mask,
-    max_words=2000, #允许最大词汇
-    max_font_size=5000 #最大号字体
-)
-word_cloud = cloud.generate(cut_text)
-plt.imshow(word_cloud)
-plt.axis('off')
-plt.show()
-word_cloud.to_file("wordcloud.jpg")
+def main():
+    comment_text = open("text.txt", 'r').read()  # txt文本
+    cut_text = ' '.join(jieba.cut(comment_text))
+    color_mask = imread("picture.jpg")  # 背景图片
+    cloud = WordCloud(
+        font_path = "msyh.ttf",  # 字体
+        background_color = 'white',  # 背景色
+        mask = color_mask,
+        max_words = 2000,  # 允许最多词汇
+        max_font_size = 5000  # 最大号字体
+    )
+    word_cloud = cloud.generate(cut_text)
+    #image_colors = ImageColorGenerator(color_mask) # 用背景颜色填充
+    #word_cloud.recolor(color_func=image_colors)
+    plt.imshow(word_cloud)
+    plt.axis('off')
+    plt.show()
+    word_cloud.to_file("wordcloud.jpg")
+
+if __name__ == '__main__':
+    main()
